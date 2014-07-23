5314247f5ed845bc79b34886d6328310f3855634
jimmypc92 <jimmypcampbell92@yahoo.com>
2014-07-23 15:04:11 -0700

    Added map to the mainpage

--- PanoramaApp1/MainPage.xaml.cs
+++ PanoramaApp1/MainPage.xaml.cs
@@ -29,5 +29,26 @@ namespace PanoramaApp1
                 App.ViewModel.LoadData();
             }
         }
+
+        /// <summary>
+        /// We must satisfy Maps API's Terms and Conditions by specifying
+        /// the required Application ID and Authentication Token.
+        /// See http://msdn.microsoft.com/en-US/library/windowsphone/develop/jj207033(v=vs.105).aspx#BKMK_appidandtoken
+        /// </summary>
+        private void MyMap_Loaded(object sender, RoutedEventArgs e)
+        {
+#if DEBUG
+#warning Please obtain a valid application ID and authentication token.
+#else
+#error You must specify a valid application ID and authentication token.
+#endif
+            Microsoft.Phone.Maps.MapsSettings.ApplicationContext.ApplicationId = "__ApplicationID__";
+            Microsoft.Phone.Maps.MapsSettings.ApplicationContext.AuthenticationToken = "__AuthenticationToken__";
+        }
+
+
+
+
+
     }
 }
